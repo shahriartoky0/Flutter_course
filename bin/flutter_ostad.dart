96c486b09ee6a8332194780fc51dc1a07d37a722
Shahriar Toky <shahriartoky555@gail.com>
2023-08-03 13:59:40 +0600

    First Assignment

---
+++ bin/flutter_ostad.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_ostad/flutter_ostad.dart' as flutter_ostad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_ostad.calculate()}!');
+}
---
+++ test/flutter_ostad_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_ostad/flutter_ostad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
